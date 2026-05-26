2400a861a261c189cead463b03b23bf6199f0462
joaokotsugai <joaokotsugai@aluno.ifsp.edu.br>
2026-05-26 08:40:22 -0300

    Card produto criado

--- lanches/src/App.jsx
+++ lanches/src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import imgComida1 from './assets/hambuguer.png'
 import imgComida2 from './assets/morango.png'
+import CardProduto from './components/CardProduto'
 
 function App() {
   const [tipoComida, setTipoComida] = useState('hambuguer');
 
-  const alterarComida = () => {
-    setTipoComida(tipoComida === 'hambuguer' ? 'morango' : 'hambuguer');
-  }
 
   const dadosComida = {
     hambuguer: {
@@ -25,11 +23,24 @@ function App() {
     }
   }
 
+    const alterarComida = () => {
+    setTipoComida(tipoComida === 'hambuguer' ? 'morango' : 'hambuguer');
+  }
+
+    const dados = dadosComida[tipoComida]
+
   return (
     <>
       <div className="container">
         <h1>Escolha a sua refeição</h1>
-        <button>Mudar Refeição</button>
+        <button onClick={alterarComida}>Mudar Refeição</button>
+        <CardProduto
+          imagem={dados.imagem}
+          titulo={dados.titulo}
+          preco={dados.preco}
+          descricao={dados.descricao}
+        />
+
       </div>
     </>
   )
